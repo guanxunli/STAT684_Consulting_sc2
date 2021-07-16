5f6c37287be04037b7c195ffbe48899278177129
Guanxun Li <victorguanxun@gmail.com>
2021-07-16 11:09:16 -0500

    Remove common part.

--- correlation_modified.R
+++ correlation_modified.R
@@ -18,7 +18,6 @@ rownames(dta_01) <- rownames(dta)
 ## spearman correlation
 spearman_mat <- matrix(NA, nrow = n, ncol = n)
 cor_01 <- matrix(NA, nrow = n, ncol = n)
-cor_common <- matrix(NA, nrow = n, ncol = n)
 
 ## Train ZINB model
 set.seed(1)
@@ -79,7 +78,6 @@ for (i in seq_len(n)) {
       }
     }
     tmp <- p00 * p11 - p01 * p10
-    cor_common[i, j] <- cor_common[j, i] <- rho
     cor_01[i, j] <- cor_01[j, i] <- tmp
     spearman_mat[i, j] <- spearman_mat[j, i] <- p11 * px1 * py1 * rho + 3 * tmp
   }
@@ -92,14 +90,11 @@ spearman_01[which(spearman_mat > 0)] <- 1
 spearman_01[which(spearman_mat < 0)] <- -1
 
 rownames(cor_01) <- colnames(cor_01) <- gene_name
-rownames(cor_common) <- colnames(cor_common) <- gene_name
 rownames(spearman_mat) <- colnames(spearman_mat) <- gene_name
 rownames(spearman_01) <- colnames(spearman_mat) <- gene_name
 
 res$cor_01 <- cor_01
-res$cor_common <- cor_common
 res$spearman_mat <- spearman_mat
-saveRDS(res, "results/res_correlation_modified.rds")
 print("Finish correlation.")
 
 ## calculate the difference pair with adjusted spearman
@@ -134,43 +129,4 @@ for (i in seq_len(length(pair_p))) {
   }
 }
 res$pair_p <- pair_p
-
-## calculate the difference pair with common part
-cor_common_01 <- matrix(0, nrow = n, ncol = n)
-cor_common_01[which(cor_common > 0)] <- 1
-cor_common_01[which(cor_common < 0)] <- -1
-
-index <- which(abs(cor_01 - cor_common_01) > 0)
-index <- setdiff(index, union(which(cor_01 == 0), which(cor_common == 0)))
-
-pair_mat <- matrix(NA, nrow = 2, ncol = length(index))
-pair_mat[1, ] <- index %/% n + 1
-pair_mat[2, ] <- index %% n
-pair_mat[2, ][which(pair_mat[2, ] == 0)] <- n
-
-gene_pair <- matrix(NA, nrow = length(index) / 2, ncol = 2)
-iter <- 1
-for (i in seq_len(length(index))) {
-  if (pair_mat[1, i] < pair_mat[2, i]) {
-    gene_pair[iter, ] <- gene_name[c(pair_mat[1, i],  pair_mat[2, i])]
-    iter <- iter + 1
-  }
-}
-res$gene_pair_common <- gene_pair
-
-## For each pair do fisher exact test
-pair_p <- rep(NA, nrow(gene_pair))
-dta_01 <- matrix(0, nrow = n, ncol = p)
-dta_01[which(dta > 0)] <- 1
-rownames(dta_01) <- rownames(dta)
-for (i in seq_len(length(pair_p))) {
-  tmp <- table(dta_01[gene_pair[i, 1], ], dta_01[gene_pair[i, 2], ])
-  if (identical(as.numeric(dim(tmp)), c(2, 2))) {
-    pair_p[i] <- fisher.test(dta_01[gene_pair[i, 1], ], dta_01[gene_pair[i, 2], ])$p.value
-  } else{
-    pair_p[i] <- 1
-  }
-}
-res$pair_p_common <- pair_p
-
-saveRDS(res, "results/res_correlation_modified.rds")
+res$gene_pair[which(pair_p < 0.05), ]
